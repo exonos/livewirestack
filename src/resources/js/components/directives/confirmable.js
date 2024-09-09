b4dd5bb634424e9ea705bf5f7bbbd54926fe2bf1
exonhos <hh.abdiel@gmail.com>
2024-09-09 16:42:50 -0700

    Fix $wire access on confirmable directive

--- src/resources/js/components/directives/confirmable.js
+++ src/resources/js/components/directives/confirmable.js
@@ -1,40 +1,13 @@
 document.addEventListener('alpine:init', () => {
-    Alpine.directive('confirmable', (el, { expression }, { evaluate, cleanup }) => {
-        let isClickable = true;
+    Alpine.directive('confirmable', (el, { expression }, { evaluate }) => {
+        // Extrae parámetros opcionales de la expresión
+        let params = expression.split(',').map(param => param.trim());
+        let title = params[0] || 'Are you sure?';
+        let message = params[1] || 'Are you sure you want to proceed?';
+        let callback = params[2] ? new Function('return ' + params[2])() : () => {};
+        let cancel = params[3] ? new Function('return ' + params[3])() : () => {};
 
-        // Función para analizar la expresión de la directiva
-        const parseExpression = (expr) => {
-            const regex = /^([^,]+)(?:,\s([^,]*))?(?:,\s([^,]*))?(?:,\s([^,]*))?$/;
-            const match = expr.match(regex);
-
-            // Asegúrate de que `match` no sea nulo
-            if (!match) {
-                return {
-                    title: 'Are you sure?',
-                    message: 'Do you want to proceed?',
-                    callback: () => {},
-                    cancel: () => {}
-                };
-            }
-
-            return {
-                title: match[1] ? match[1].trim() : 'Are you sure?',
-                message: match[2] ? match[2].trim() : 'Do you want to proceed?',
-                callback: match[3] ? new Function('return ' + match[3])() : () => {},
-                cancel: match[4] ? new Function('return ' + match[4])() : () => {}
-            };
-        };
-
-        const { title, message, callback, cancel } = parseExpression(expression);
-
-        const onClick = async (e) => {
-            e.preventDefault();
-            e.stopImmediatePropagation();
-
-            if (!isClickable) return;
-            isClickable = false;
-
-            // Crear la promesa para el diálogo de confirmación
+        el.addEventListener('click', async () => {
             let result = await new Promise(resolve => {
                 window.dispatchEvent(new CustomEvent('confirm', {
                     detail: {
@@ -55,17 +28,6 @@ document.addEventListener('alpine:init', () => {
             if (result) {
                 evaluate();
             }
-
-            // Restaurar el estado de clickeo
-            setTimeout(() => {
-                isClickable = true;
-            }, 1000);
-        };
-
-        el.addEventListener('click', onClick, { capture: true });
-
-        cleanup(() => {
-            el.removeEventListener('click', onClick);
         });
     });
 });
